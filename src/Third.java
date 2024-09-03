8973f5842de393d70eebc86f25e1b970525f9fad
aravindphs <aravind94977034@gmail.com>
2024-09-03 15:20:21 +0530

    doneTest

---
+++ src/Third.java
@@ -0,0 +1,11 @@
+public class Third implements First, Second{
+
+    public void firstMethod() {
+        System.out.println("Inside First");
+    }
+
+
+    public void secondMethod() {
+        System.out.println("Inside Second");
+    }
+}
